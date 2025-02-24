c9054cc0cf3f11ab1def4fe7c573c1d0477ffed5
vsegda_vinovat <riabko97@gmail.com>
2025-02-24 14:35:49 +0200

    added session decorator

---
+++ src/auth/session-info.decorator.ts
@@ -0,0 +1,6 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+export const SessionInfo = createParamDecorator((
+    _, ctx: ExecutionContext) => ctx.switchToHttp().getRequest().session
+
+)
