f9b21737c4df58861482e71d5418a7e28ca21645
vsegda_vinovat <riabko97@gmail.com>
2025-02-24 22:44:53 +0200

    registered pipe

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser'
+import { ValidationPipe } from '@nestjs/common';
 
 
 async function bootstrap() {
@@ -13,7 +14,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, docs)
 
   app.use(cookieParser())
-
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(process.env.PORT ?? 3000);
 }
