6687a4548ae460917c742e6f0896501fdd451245
vsegda_vinovat <riabko97@gmail.com>
2025-02-25 19:03:51 +0200

    import blocklist module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { DbModule } from 'src/db/db.module';
 import { AccountModule } from 'src/account/account.module';
+import { BlockListModule } from 'src/block-list/block-list.module';
 
 @Module({
-  imports: [DbModule, AccountModule],
+  imports: [DbModule, AccountModule, BlockListModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
