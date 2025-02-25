433c1d39440fb0a5789cc118591645c3208f00a8
vsegda_vinovat <riabko97@gmail.com>
2025-02-25 19:02:17 +0200

    added block list controller

--- src/block-list/block-list.controller.ts
+++ src/block-list/block-list.controller.ts
@@ -1,7 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Query, UseGuards } from '@nestjs/common';
 import { BlockListService } from './block-list.service';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { AddBlockItemDto, BlockItemDto, BlockListDto, BlockListQueryDto } from './dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { SessionInfo } from 'src/auth/session-info.decorator';
+import { GetSessionDto } from 'src/auth/dto';
 
 @Controller('block-list')
+@UseGuards(AuthGuard)
 export class BlockListController {
-  constructor(private readonly blockListService: BlockListService) {}
+  constructor(private readonly blockListService: BlockListService) { }
+
+
+  @Get()
+  @ApiOkResponse({
+    type: BlockListDto
+  })
+  getList(@Query() query: BlockListQueryDto,
+    @SessionInfo() session: GetSessionDto): Promise<BlockListDto> {
+
+    return this.blockListService.getByUser(session.id, query)
+  }
+
+
+  @Post('item')
+  @ApiCreatedResponse({
+    type: AddBlockItemDto
+  })
+  addBlockItem(
+    @Body() body: AddBlockItemDto,
+    @SessionInfo() session: GetSessionDto): Promise<BlockItemDto> {
+
+    return this.blockListService.addItem(session.id, body)
+  }
+
+  
+  @Delete('item/:id')
+  @ApiOkResponse({
+    type: BlockItemDto
+  })
+  removeBlockItem(
+    @Param('idn', ParseIntPipe) id: number,
+    @SessionInfo() session: GetSessionDto
+  ): Promise<BlockItemDto> {
+    return this.blockListService.removeItem(session.id, id)
+  }
 }
