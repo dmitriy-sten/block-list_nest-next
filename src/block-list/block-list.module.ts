398949388c34ef17c3277c2e904b0c2b41de290c
vsegda_vinovat <riabko97@gmail.com>
2025-02-25 19:02:47 +0200

    imported db module and exported blocklist service

--- src/block-list/block-list.module.ts
+++ src/block-list/block-list.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BlockListService } from './block-list.service';
 import { BlockListController } from './block-list.controller';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [BlockListController],
   providers: [BlockListService],
+  exports: [BlockListService]
 })
-export class BlockListModule {}
+export class BlockListModule { }
