293bf991ea0341cdac273bb9350d4b68e083ca23
vsegda_vinovat <riabko97@gmail.com>
2025-02-25 12:53:22 +0200

    added account service

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,17 +1,41 @@
 import { Injectable, UseGuards } from '@nestjs/common';
 import { PatchAccountDto } from './dto';
+import { DbService } from 'src/db/db.service';
 
 @Injectable()
 export class AccountService {
+    constructor(private db: DbService) { }
 
-
-
-    getAccount(userId: number) {
-
+    async create(userId: number) {
+        return this.db.account.create({
+            data: {
+                ownerId: userId,
+                isBlockingEnabled: false
+            }
+        })
     }
 
 
-    patchAccount(userId: number, patch: PatchAccountDto) {
-
+    async getAccount(userId: number) {
+        return await this.db.account.findFirstOrThrow({
+            where: {
+                ownerId: userId
+            }
+        })
     }
+
+    async patchAccount(userId: number, patch: PatchAccountDto) {
+        return await this.db.account.update({
+            where: {
+                ownerId: userId
+            },
+            data: {
+                isBlockingEnabled: patch.isBlockingEnabled
+            }
+        })
+    }
+
+
+
+
 }
