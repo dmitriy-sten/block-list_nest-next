a0560ce4f8b0b486595516bf9fe73ed6248e59a2
vsegda_vinovat <riabko97@gmail.com>
2025-02-25 12:47:38 +0200

    added import and exports

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [AccountController],
   providers: [AccountService],
+  exports: [AccountService]
 })
-export class AccountModule {}
+export class AccountModule { }
