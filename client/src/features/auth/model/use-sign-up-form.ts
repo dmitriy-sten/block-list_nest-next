2b9ea88c4bc3cc2cc65ec09a8b02f82c5bf06456
vsegda_vinovat <riabko97@gmail.com>
2025-02-27 21:42:50 +0200

    added sign-up

---
+++ client/src/features/auth/model/use-sign-up-form.ts
@@ -0,0 +1,42 @@
+import { authControllerSignUp } from "@/shared/api/generated";
+import { ROUTES } from "@/shared/constants/routes";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+
+
+
+export const useSignUpForm = () => {
+    const { push } = useRouter();
+
+    const {
+        reset,
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm<{
+        email: string;
+        password: string;
+    }>({
+        mode: "onBlur",
+    });
+
+    const signUpMutation = useMutation({
+        mutationFn: authControllerSignUp,
+        onSuccess: () => {
+            reset(), push(ROUTES.home);
+        },
+    });
+
+    const errorServerMessage = signUpMutation?.error
+        ? signUpMutation?.error.response?.data?.message : undefined
+
+    return {
+        register, reset, signUpMutation,
+        handleSubmit: handleSubmit(data => signUpMutation.mutate(data)),
+        errors,
+        errorServerMessage
+
+    }
+}
--- client/src/features/auth/ui/sign-up-form.tsx
+++ client/src/features/auth/ui/sign-up-form.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+"use client";
 
-interface Props {
-    className?: string;
-}
+import { UiButton } from "@/shared/ui/ui-button";
+import { UiTextField } from "@/shared/ui/ui-textfield";
+import React from "react";
+import { useSignUpForm } from "../model/use-sign-up-form";
+import { UiLink } from "@/shared/ui/ui-link";
+import { ROUTES } from "@/shared/constants/routes";
 
-export const SignUpForm: React.FC<Props> = ({ className }) => {
+export const SignUpForm: React.FC = () => {
+  const { handleSubmit, register, signUpMutation, errors, errorServerMessage } =
+    useSignUpForm();
+
   return (
-    <div className={className}></div>
+    <form onSubmit={handleSubmit} action="" className="flex flex-col gap-3">
+      <UiTextField
+        inputProps={{
+          type: "email",
+          ...register("email", { required: "Required" }),
+        }}
+        label="Email"
+        error={errors?.email?.message}
+      />
+      <UiTextField
+        inputProps={{
+          type: "password",
+          ...register("password", { required: "Required" }),
+        }}
+        label="Password"
+        error={errors?.password?.message}
+      />
+      <UiButton
+        disabled={signUpMutation.isPending}
+        type="submit"
+        variant="primary"
+      >
+        {signUpMutation.isPending ? "Enter..." : "Sign Up"}
+      </UiButton>
+      <p className="text-red-400 text-center">{errorServerMessage}</p>
+      <UiLink className="text-center" href={ROUTES.signIn}>
+        Sign In
+      </UiLink>
+    </form>
   );
 };
