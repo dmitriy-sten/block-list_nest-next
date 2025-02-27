3076cb6ce976958fe04b81ee182b2aa14ac4ef40
vsegda_vinovat <riabko97@gmail.com>
2025-02-27 19:05:07 +0200

    added right side slot to header

--- client/src/shared/ui/ui-header.tsx
+++ client/src/shared/ui/ui-header.tsx
@@ -4,17 +4,19 @@ import { UiLogo } from "./ui-logo";
 
 interface Props {
   className?: string;
+  right?: React.ReactNode;
 }
 
-export const UiHeader: React.FC<Props> = ({ className }) => {
+export const UiHeader: React.FC<Props> = ({ className, right }) => {
   return (
     <header
       className={clsx(
-        "w-screen px-4 border-b border-slate-300 flex justify-between",
+        " px-4 py-5 border-b bg-white border-slate-300 flex justify-between items-center",
         className
       )}
     >
       <UiLogo className="size-10" />
+      {right}
     </header>
   );
 };
