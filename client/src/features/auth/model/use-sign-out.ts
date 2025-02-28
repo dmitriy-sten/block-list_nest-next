242a70b5e39b1c12b3aaa099334be6a269c990fb
vsegda_vinovat <riabko97@gmail.com>
2025-02-28 14:50:22 +0200

    added reset hook

--- client/src/features/auth/model/use-sign-out.ts
+++ client/src/features/auth/model/use-sign-out.ts
@@ -1,9 +1,10 @@
 
 
 
+import { useResetSession } from "@/entities/session/queries"
 import { authControllerSignOut } from "@/shared/api/generated"
 import { ROUTES } from "@/shared/constants/routes"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
 
 
@@ -12,12 +13,13 @@ import { useRouter } from "next/navigation"
 export const useSignOut = () => {
 
     const router = useRouter()
-
+    const resetSession = useResetSession()
 
     const signOutMutation = useMutation({
         mutationFn: authControllerSignOut,
         onSuccess: () => {
             router.replace(ROUTES.signIn)
+            resetSession()
         }
     })
 
