f2a159a8e8aec4b572570021d379cc49ac6ffb74
vsegda_vinovat <riabko97@gmail.com>
2025-02-28 14:49:12 +0200

    small refactor

--- client/src/app/_providers/protected-provider.tsx
+++ client/src/app/_providers/protected-provider.tsx
@@ -5,26 +5,32 @@ import { authControllerGetSessionInfo } from "@/shared/api/generated";
 import { UiPageSpinner } from "@/shared/ui/ui-page-spinner";
 import { useQuery } from "@tanstack/react-query";
 import React, { ReactNode } from "react";
+import { useRouter } from "next/navigation";
+import { ROUTES } from "@/shared/constants/routes";
+import { useSessionQuery } from "@/entities/session/queries";
 
 interface Props {
   children: ReactNode;
 }
 
 export const ProtectedProvider: React.FC<Props> = ({ children }) => {
-  const { isSuccess, isLoading } = useQuery({
-    queryKey: ["session"],
-    queryFn: authControllerGetSessionInfo,
-  });
+  const router = useRouter();
 
-  console.log("query");
-
+  const { isLoading, isError } = useSessionQuery();
+  
   if (isLoading) {
     return <UiPageSpinner />;
   }
-
-  if (isSuccess) {
-    return <>{children}</>;
+  if (isError) {
+    router.replace(ROUTES.signIn);
   }
 
-  return null;
+  return children;
 };
+
+// export const ProtectedProvider = dynamic(
+//   () => Promise.resolve(ProtectedProviderComponent),
+//   {
+//     ssr: false,
+//   }
+// );
