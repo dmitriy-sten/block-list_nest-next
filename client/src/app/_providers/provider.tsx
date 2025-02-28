e32290ecf308753c4d0a2795085e34f875db0360
vsegda_vinovat <riabko97@gmail.com>
2025-02-28 14:50:01 +0200

    provider settled to only csr

--- client/src/app/_providers/provider.tsx
+++ client/src/app/_providers/provider.tsx
@@ -3,14 +3,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import React, { useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 interface Props {
   children: React.ReactNode;
 }
 
-
 interface ApiError {
-  message:string
+  message: string;
 }
 
 type AxiosApiError = AxiosError<ApiError>;
@@ -20,8 +20,6 @@ declare module "@tanstack/react-query" {
     defaultError: AxiosApiError;
   }
 }
-
-
 
 export const Provider: React.FC<Props> = ({ children }) => {
   const [client] = useState(
@@ -36,5 +34,10 @@ export const Provider: React.FC<Props> = ({ children }) => {
       })
   );
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
 };
